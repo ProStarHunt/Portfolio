ad0ec33fc6a64d10ba4cf840828c48cd09674652
ProStarHunt <businesspsh123@gmail.com>
2025-02-07 13:35:54 +0530

    Add files via upload

---
+++ work.js
@@ -0,0 +1,9 @@
+export default function Work() {
+    return (
+      <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
+        <h1 className="text-5xl font-bold">My Work</h1>
+        <p className="mt-4 text-lg text-gray-400">Showcase of my projects</p>
+      </div>
+    );
+  }
+  
