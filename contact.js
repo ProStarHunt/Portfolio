6fe4693db0393315ad49e9f1537ceb5ab56a4198
ProStarHunt <businesspsh123@gmail.com>
2025-02-08 15:26:32 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,9 +0,0 @@
-export default function Contact() {
-    return (
-      <div className="min-h-screen bg-black text-white flex flex-col items-center justify-center">
-        <h1 className="text-5xl font-bold">Contact Me</h1>
-        <p className="mt-4 text-lg text-gray-400">Email: your.email@example.com</p>
-      </div>
-    );
-  }
-  
